5384dbd7ece5970b6c5d7e1c0541249abfab3744
Dutkercz <dutkercz@gmail.com>
2025-05-11 07:48:37 -0300

    Primeiro commit

---
+++ src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "hello-world", //selector é uma TAG E template é o texto html
+    template : "<h1>Hello World e SAI MALDIÇÃO</h1>"
+})
+export class HelloWorldComponent {
+
+}
