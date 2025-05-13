e36e039d837cb73b56d5a0b2b82d75b87052493c
Dutkercz <dutkercz@gmail.com>
2025-05-13 05:24:41 -0300

    Segundo dia - calculadora de soma, e inicio da lista de compras

--- src/app/helloworld/helloworld.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "hello-world", //selector é uma TAG E template é o texto html
-    template : "<h1>Hello World e SAI MALDIÇÃO</h1>"
-})
-export class HelloWorldComponent {
-
-}
